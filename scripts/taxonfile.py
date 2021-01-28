0525f7d1fbc857c64127a804962a3d4e0309cc93
Maria del Mar Quiroga <mar.quiroga@unimelb.edu.au>
2021-01-28 16:20:19 +1100

    Make dictionary for taxonomic OTU file, and update python script
    Former-commit-id: b14c51be80126630ddf0581db308727b010f5fa7
    Former-commit-id: 594af999a7fe316e49017e0e239bad769a419ca2
    Former-commit-id: 92b6fb91854d9912acf53505757c68f18f363cf9

---
+++ scripts/taxonfile.py
@@ -0,0 +1,26 @@
+import shelve
+import json
+
+with open('/data/gpfs/projects/punim0639/databases/metaGenePipe/taxon/br08610.json') as f:
+	tax = json.load(f)
+
+def get_lineage(tree, current = []):
+    if "children" in tree:
+        for child in tree["children"]:
+            if isinstance(child, dict):
+                yield from get_lineage(child, current+[tree["name"]])
+            elif isinstance(child, list):
+                for i in child:
+                    yield from get_lineage(i, current+[tree["name"]])
+    else:
+        code = tree["name"].split()[0]
+        yield [code]+current+[tree["name"]]
+
+eukaryota = list(get_lineage(tax['children'][0]))
+bacteria = list(get_lineage(tax['children'][1]))
+archaea = list(get_lineage(tax['children'][2]))
+
+my_dict = {lineage[0]: "; ".join(lineage[1:]) for lineage in eukaryota+bacteria+archaea}
+
+myShelvedDict = shelve.open("/data/gpfs/projects/punim0639/databases/metaGenePipe/taxon/taxonomic_lineages.db")
+myShelvedDict.update(my_dict)
---
+++ scripts/xml_parser-v4.py
@@ -0,0 +1,51 @@
+import xml.etree.ElementTree as ET
+import sys
+import re
+import argparse
+import pandas as pd
+import shelve
+
+parser = argparse.ArgumentParser()
+parser.add_argument('xmls', help="The XML files to search for the hits.",  nargs='+')
+parser.add_argument('--outfile', help="The name of the output file. Default: OTU.tsv", default="OTU.tsv")
+
+args = parser.parse_args()
+
+tax = shelve.open('/data/gpfs/projects/punim0639/databases/metaGenePipe/taxon/taxonomic_lineages.db')
+
+df = pd.DataFrame()
+
+sample_names = []
+for xml in args.xmls:
+	sample_name = xml.split(".")[0]
+	sample_names.append(sample_name)
+
+	root = ET.parse(xml).getroot()
+	for hit in root.findall("./BlastOutput_iterations/Iteration/Iteration_hits/Hit"):
+		hit_text = hit.find("Hit_id").text
+		code = hit_text.split(":")[0]
+		df = df.append( {'code':code, "sample_name":sample_name}, ignore_index=True)
+
+print(f"Writing to {args.outfile}")
+with open(args.outfile, "w") as f:
+	columns = ["Name", "Kegg Code", "Lineage"] + sample_names
+	print("\t".join(columns), file=f)
+
+	codes = sorted(df.code.unique())
+
+	for code in codes:
+		if code in tax.keys():
+			lineage = tax[code]
+			name = lineage.split(";")[-1]
+			name = name[ len(code)+2:].strip()
+		else:
+			lineage = "unknown"
+			name = "Unknown"
+
+		f.write( "\t".join( [name, code, lineage] )  )
+		for sample_name in sample_names:
+			filtered = df[ (df.sample_name == sample_name) & (df.code == code) ]
+			count = len(filtered.index)
+			f.write( f"\t{count}" )
+
+		f.write("\n")
